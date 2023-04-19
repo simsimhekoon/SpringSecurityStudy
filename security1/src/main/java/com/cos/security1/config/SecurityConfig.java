032b8a34be0bfc6f2ce19b26daf741ce6879eacd
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2023-04-19 14:37:33 +0900

    2강 정리
    2강 정리 - 내용은 강의내용요약 폴더의 파일 참조

---
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+                .requestMatchers("/user/**").authenticated()
+                .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")
+                .requestMatchers("/amdin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login") // login주소가 호출되면 시큐리티가 낚아챔. -> controller에 매핑안해도됨.
+                .defaultSuccessUrl("/"); //로그인 완료 후, 리다이렉트
+        return http.build();
+    }
+}
+
--- security1/src/main/java/com/cos/security1/controller/indexController.java
+++ security1/src/main/java/com/cos/security1/controller/indexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 리턴하겠다는 뜻
 public class indexController {
@@ -12,5 +13,35 @@ public class indexController {
     public String index() {
         //뷰리졸버 설정 : templates(prefix), .html(suffix) => index.config
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    //기본적으로 "/login"은 스프링 시큐리티가 낚아챔.
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨!";
     }
 }
